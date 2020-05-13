313dcb17b30f62c2d1ca6b23ed7cef7ecc42de29
Volodymyr Yepishev <i.m.bwca@gmail.com>
2020-05-14 00:41:41 +0300

    fixed

--- Main.gd
+++ Main.gd
@@ -2,8 +2,11 @@ extends Node
 
 export (PackedScene) var Mob
 var score
+var screen_size = OS.get_screen_size()
+var window_size = OS.get_window_size()
 
 func _ready():
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
 	randomize()
 
 func _on_Player_hit():
--- Player.gd
+++ Player.gd
@@ -4,50 +4,66 @@ signal hit
 
 export var speed = 400
 var screen_size
+# Add this variable to hold the clicked position.
+var target = Vector2()
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size
 	hide()
+	screen_size = get_viewport_rect().size
 
+func start(pos):
+	position = pos
+	# Initial target is the start position.
+	target = pos
+	show()
+	$CollisionShape2D.disabled = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Change the target whenever a touch event happens.
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+
 func _process(delta):
 	var velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	# Move towards the target and stop when close.
+	if position.distance_to(target) > 10:
+		velocity = (target - position).normalized() * speed
+	else:
+		velocity = Vector2()
+
+# Remove keyboard controls.
+#    if Input.is_action_pressed("ui_right"):
+#       velocity.x += 1
+#    if Input.is_action_pressed("ui_left"):
+#        velocity.x -= 1
+#    if Input.is_action_pressed("ui_down"):
+#        velocity.y += 1
+#    if Input.is_action_pressed("ui_up"):
+#        velocity.y -= 1
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
-		
+
 	position += velocity * delta
+	# We still need to clamp the player's position here because on devices that don't
+	# match your game's aspect ratio, Godot will try to maintain it as much as possible
+	# by creating black borders, if necessary.
+	# Without clamp(), the player would be able to move under those borders.
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right"
 		$AnimatedSprite.flip_v = false
-		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
-
-func _on_Player_body_entered(body):
+func _on_Player_body_entered( body ):
 	hide()
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
